69702e258bc72b012e94b65baa3a46a3890c293b
Bang04 <wjdtjr92@naver.com>
2025-09-15 16:44:36 +0900

    커리어 프로젝트 기능,내용 수정

--- src/components/CareerProject.tsx
+++ src/components/CareerProject.tsx
@@ -6,89 +6,92 @@ import { ImageSlide } from "./ImageSlide";
 import careerProjects from "../data/careerProjects";
 
 export const CareerProject = () => {
-    const [hovered, setHovered] = useState<boolean[]>(()=> careerProjects.map(()=> false));
+    const [hovered, setHovered] = useState<boolean[]>(() => careerProjects.map(() => false));
 
-    return(
+    return (
         <div className="flex flex-col items-center">
             <div className="flex flex-col mb-20">
                 <div className="w-4/5 mx-auto p-3 text-lg text-left font-bold">🚀 주요성과</div>
                 <div className="flex flex-col gap-10">
-                { careerProjects.map((project, i: number) => {
-                    return (
-                        <motion.div
-                            onMouseEnter={() => setHovered(prev =>{
+                    {careerProjects.map((project, i: number) => {
+                        return (
+                            <motion.div
+                                onMouseEnter={() => setHovered(prev => {
                                     const newHover = [...prev];
                                     newHover[i] = true;
                                     return newHover
                                 })
-                            }
-                            // onMouseLeave={() => setHovered(prev => {
-                            //         const newHover = [...prev];
-                            //         newHover[i] = false;                 
-                            //         return newHover
-                            //     })
-                            // }
-                            initial={{ opacity: 0, y: 0 }}
-                            animate={{ opacity: 1, y: 0 }}  // 스크롤 들어올 때만 애니메이션 실행
-                            transition={{ duration: 0.6, ease: "easeOut" }}
-                            className="relative flex flex-col md:flex-row w-5/6 m-auto rounded-2xl shadow-lg bg-white"
-                        >
-                            {/* 이미지 영역 */}
-
-                            <ImageSlide />
-                            {/* <motion.div
-                                transition={{ duration: 0.5, ease: "easeInOut" }}
-                                className="flex items-center w-full scroll-smooth rounded-2xl"
+                                }
+                                // onMouseLeave={() => setHovered(prev => {
+                                //         const newHover = [...prev];
+                                //         newHover[i] = false;                 
+                                //         return newHover
+                                //     })
+                                // }
+                                initial={{ opacity: 0, y: 0 }}
+                                animate={{ opacity: 1, y: 0 }}  // 스크롤 들어올 때만 애니메이션 실행
+                                transition={{ duration: 0.6, ease: "easeOut" }}
+                                className="relative flex flex-col md:flex-row w-5/6 m-auto rounded-2xl shadow-lg bg-white"
                             >
-                                <img
-                                    src={project.image}
-                                    alt="example"
-                                    className="w-full object-top object-cover  rounded-2xl"
-                                />
-                            </motion.div> */}
-
-                            {/* 설명 영역 */}
-                            <motion.div
-                                initial={{ opacity: 0 }}
-                                style={{ height :  hovered[i]? "auto" :"0px"  }}
-                                animate={{ opacity: hovered[i] ? 1 : 0  , flexBasis: hovered[i] ? "100%" : "0%" }}
-                                transition={{ duration: 0.5, ease: "easeInOut" }}
-                                className=" flex justify-center bg-white rounded-2xl overflow-hidden overflow-y-scroll"
-                            >
-                                {/* /items-center  */}
+                                {/* 이미지 영역 */}
+                                <ImageSlide images={project.image} mode={project.mode} />
+                                {/* 설명 영역 */}
                                 <motion.div
-                                    animate={{ opacity: hovered[i] ? 1 : 0 }}
-                                    transition={{ duration: 0.3, delay: hovered[i] ? 0.2 : 0 }}
-                                    className="px-6 py-2 text-gray-700 "
+                                    initial={{ opacity: 0 }}
+                                    style={{ height: hovered[i] ? "auto" : "0px" }}
+                                    animate={{ opacity: hovered[i] ? 1 : 0, flexBasis: hovered[i] ? "100%" : "0%" }}
+                                    transition={{ duration: 0.5, ease: "easeInOut" }}
+                                    className="flex justify-center bg-white rounded-2xl overflow-hidden overflow-y-scroll"
                                 >
-                                    <div className="text-xl font-bold text-gray-800">{project.title}</div>
-                                    <div className="text-sm text-gray-500"><b className="mr-2">web</b>{project.periods.web}</div>
-                                        <div className="text-sm text-gray-500"><b className="mr-2">app</b>{project.periods.app}</div>
-                                    <div className="text-base text-gray-700 mt-2">{project.role}</div>
-                                    <div>
-                                        <div className="text-lg font-semibold text-gray-800 mt-4">역할 / 업무성과</div>
-                                        <div className="text-sm text-gray-500">
-                                            <ul className="list-disc list-inside text-sm text-gray-600 space-y-1 mt-2">
-                                                { project.achievements.map((achiev) => (
-                                                    <li>{achiev}</li>
+                                    {/* /items-center  */}
+                                    <motion.div
+                                        animate={{ opacity: hovered[i] ? 1 : 0 }}
+                                        transition={{ duration: 0.3, delay: hovered[i] ? 0.2 : 0 }}
+                                        className="px-6 py-2 text-gray-700 "
+                                    >
+                                        <div className="text-xl font-bold text-gray-800">{project.title}</div>
+                                        {project.periods.web !== "" ?
+                                            <div className="text-sm text-gray-500">
+                                                <b className="mr-2">web</b>
+                                                {project.periods.web}
+                                            </div>
+                                            : 
+                                            ""
+                                        }
+                                        {project.periods.app !== "" ?
+                                            <div className="text-sm text-gray-500">
+                                                <b className="mr-2">app</b>
+                                                {project.periods.app}
+                                            </div>
+                                            : 
+                                            ""
+                                        }
+                                        <div className="text-base text-gray-700 mt-2">{project.role}</div>
+                                        <div>
+                                            <div className="text-lg font-semibold text-gray-800 mt-4">역할 / 업무성과</div>
+                                            <div className="text-sm text-gray-500">
+                                                <ul className="list-disc list-inside text-sm text-gray-600 space-y-1 mt-2">
+                                                    {project.achievements.map((achiev) => (
+                                                        <li>{achiev}</li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                            <div className="flex flex-wrap gap-1 mt-4">
+                                                {project.techStack.map((sill) => (
+                                                    <SkillTag name={sill} />
                                                 ))}
-                                            </ul>
+                                            </div>
                                         </div>
-                                        <div className="flex flex-wrap gap-1 mt-4">
-                                            {project.techStack.map((sill) =>(
-                                                <SkillTag name={sill} />
-                                            ))}
-                                        </div>
-                                    </div>
+                                    </motion.div>
                                 </motion.div>
-                            </motion.div>
 
-                            
-                        </motion.div>
-                    )})
-                }   
+
+                            </motion.div>
+                        )
+                    })
+                    }
                 </div>
-            </div>  
+            </div>
         </div>
     )
 }
--- src/data/careerProjects.ts
+++ src/data/careerProjects.ts
@@ -1,20 +1,46 @@
 
-import solMain from "../assets/images/solariver/w_main.png";
-import nremsLogin from "../assets/images/nrems/m_login.png";
+import nrems_login from "../assets/images/nrems/nrems_login.png";
+import nrems_main from "../assets/images/nrems/nrems_main.png";
+import nrems_g1 from "../assets/images/nrems/nrems_g1.png";
+import nrems_g2 from "../assets/images/nrems/nrems_g2.png";
+
+import w_main from "../assets/images/solariver/w_main.png";
+import w_pw_day_d from "../assets/images/solariver/w_pw_day_d.png";
+import w_pw_day_g from "../assets/images/solariver/w_pw_day_g.png";
+import w_re_inv from "../assets/images/solariver/w_re_inv.png";
+
+import m_login from "../assets/images/solariver/m_login.png";
+import m_create from "../assets/images/solariver/m_create.png";
+import m_module from "../assets/images/solariver/m_module.png";
+
 
 export const careerProjects = [
   {
-    title: "솔라리버 태양광 모니터링 Web & 솔라리버 QR Scanner App (Hybrid APP)",
+    title: "솔라리버 태양광 모니터링 Web",
     periods: {
       web: "2022.08 ~ 2023.01 (약 6개월)",
-      app: "2022.12 ~ 2023.04 (약 5개월)"
+      app: ""
     },
-    image: solMain,
+    mode: "web",
+    image: [ w_main,w_pw_day_d,w_pw_day_g,w_re_inv ],
     role: "화면설계, 퍼블리싱, 화면 개발 (80%)",
-    techStack: ["HTML", "CSS", "JavaScript", "Ajax", "jQuery", "PHP", "MySQL", "Android"],
+    techStack: ["HTML", "CSS", "JavaScript", "Ajax", "jQuery", "PHP", "MySQL"],
     achievements: [
       "AmChart를 이용한 태양광 발전량 데이터 시각화",
-      "발전량 데이터를 엑셀 파일 다운로드 및 프린트 기능 구현",
+      "발전량 데이터를 엑셀 파일 다운로드 및 프린트 기능 구현"
+    ]
+  },
+   {
+    title: "솔라리버 QR Scanner App (Hybrid APP)",
+    periods: {
+      web: "",
+      app: "2022.12 ~ 2023.04 (약 5개월)"
+    },
+    mode: "mobile",
+    image: [ m_login,m_create,m_module ],
+    role: "개발, 배포 (100%)",
+    techStack: ["HTML", "CSS", "JavaScript", "Ajax", "jQuery", "PHP", "MySQL", "Android"],
+    achievements: [
       "QR 스캐너 라이브러리를 이용한 모뎀 등록 및 관리 가능한 하이브리드 앱 개발",
       "Android App 배포 경험"
     ]
@@ -25,7 +51,8 @@ export const careerProjects = [
       web: "2022.01 ~ 2022.03 (약 3개월)",
       app: "2022.03 ~ 2022.06 (약 3개월)"
     },
-    image: nremsLogin,
+     mode: "mobile",
+    image: [ nrems_login,nrems_main,nrems_g1,nrems_g2 ],
     role: "기본 기능 버그 수정 및 인터페이스 기능 개선 (50%)",
     techStack: [
       "HTML", "CSS",  "JavaScript", "Ajax", "jQuery", "PHP","MySQL", "Android"
