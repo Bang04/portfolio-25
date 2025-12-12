a261ae8481baa90a5e7e56907c46d4867f153bbf
Bang04 <wjdtjr92@naver.com>
2025-12-12 15:16:24 +0900

    docs : add project demo link

--- src/data/portfolioProjects.ts
+++ src/data/portfolioProjects.ts
@@ -125,7 +125,7 @@ const projects = [
     ],
     skills: ["HTML", "CSS", "JavaScript", "React","Tailwind"],
     links: { 
-      demo: null,   // 배포 사이트
+      demo: "b-sunnyside.vercel.app",   // 배포 사이트
       github: "https://github.com/Bang04/frontend-mentor-solo/tree/a8f24fc04ac8e7788a8ec5fb985f1f430d8d54ac/sunnyside-agency-landing-page"
     }
   },
